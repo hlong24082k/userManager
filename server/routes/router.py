a04465a0345808fa89814c3f13571de218adcd16
hlong24082k <hlong.24082k@gmail.com>
2024-04-04 19:50:13 +0700

    chore: delete server

--- server/routes/router.py
+++
@@ -1,8 +0,0 @@
-import fastapi
-from routes import login, product, user
-
-router = fastapi.APIRouter()
-
-router.include_router(login.router, tags = ["Login"])
-router.include_router(product.router, tags = ["Product"])
-router.include_router(user.router, tags = ["User"])
